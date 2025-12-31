0e890e3b3c071863a48884031302d646eada7ee6
Goshty4234 <voilanicolas@gmail.com>
2025-12-31 02:11:59 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import OpportunityCostCalculator from './components/OpportunityCostCalculator'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <OpportunityCostCalculator />
+    </div>
+  )
+}
+
+export default App
+
+
+
